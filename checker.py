68c6d96ac4b3a6c8177d4d75717163ca7846df90
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2017-12-08 17:13:11 +0000

    wip

--- checker.py
+++ checker.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
+import tkinter as tk
+# import tkMessageBox
 import urllib.request
+from hello_world import Application
+
+
+root = tk.Tk()
+app = Application(master=root)
+
+
+
 
 url = 'http://example.com'
 
 try:
     response = urllib.request.urlopen(url)
+    app.mainloop()
 
     print(response.getcode())
 except urllib.error.HTTPError as e:
